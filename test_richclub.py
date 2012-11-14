3f4b2216853321c1324c47f1877bdb6b0a4c259a
Jeff <jeffalstott@gmail.com>
2012-11-14 19:43:28 +0000

    Bug checking.

--- test_richclub.py
+++ test_richclub.py
@@ -60,6 +60,8 @@ class FirstTestCase(unittest.TestCase):
                     mode = 1
                 if preserve == 'in':
                     mode = 2
+                if directed:
+                    mode = 3
                 print sort(g.strength(mode=mode, weights=g.es["weight"]))
                 print sort(gr.strength(mode=mode, weights=gr.es["weight"]))
 
